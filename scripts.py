887e28af6f031580493caa4453a925ed5488f08b
manu-sugnana <Manu.Sugnana.Prasad@ndow.org>
2025-11-12 11:15:50 -0800

    Add multiplication operation to calculator script

--- scripts.py
+++ scripts.py
@@ -1,11 +1,14 @@
 
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
-op = input("Enter + or -: ")
+op = input("Enter + or - or *: ")
 
 if op == "+":
     print("Result:", a + b)
 elif op == "-":
     print("Result:", a - b)
+elif op == "-":
+    print("Result:", a * b)
 else:
     print("Invalid operator")
+
